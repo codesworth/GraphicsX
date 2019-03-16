f07610c6b38c6f65a212547b1f02cdaefa21ef02
Codesworth <codezworth@gmail.com>
2019-03-16 00:45:23 +0000

    Issues with glewCreateWindow. Looking for Fix

--- Hyllyce/Hyllyce/main.cpp
+++ Hyllyce/Hyllyce/main.cpp
@@ -7,9 +7,43 @@
 //
 
 #include <iostream>
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+void init(GLFWwindow * window){}
+
+void display(GLFWwindow* window, double currenttime){
+    glClearColor(1.9, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+    
+    if (!glfwInit()) { exit(EXIT_FAILURE);}
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "Hyllyce",glfwGetPrimaryMonitor(), GLFWwindow *share)
+    glfwMakeContextCurrent(window);
+    
+    if (glewInit() != GLEW_OK) { exit(EXIT_FAILURE);}
+    
+    glfwSwapInterval(1);
+    
+    init(window);
+    
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+        
+        
+    }
+    
+    glfwDestroyWindow(window);
+    
+    glfwTerminate();
+    
+    exit(EXIT_SUCCESS);
+    
 }
